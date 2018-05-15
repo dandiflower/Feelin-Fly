0b5c3205e1bacf665540bdcaa8e58553ae828d48
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-05-14 20:26:14 -0700

    add ajax calls

---
+++ assets/javascript/feelin-fly.js
@@ -0,0 +1,21 @@
+  //feelinFly API
+  // var queryURL = "https://feelinfly.com/watson"
+
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "POST",
+  //   data: {text: ""}
+  // }).then(function(response) {
+  //   console.log(response)
+  // });
+
+  // //feelinFly API
+  // var queryURL = "https://feelinfly.com/countries"
+
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "POST",
+  //   data: {text: ""}
+  // }).then(function(response) {
+  //   console.log(response)
+  // });
