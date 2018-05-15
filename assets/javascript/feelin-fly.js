f09adce5d4b3e5d593eed42ce1f24a15e7fd40fd
Jessica Brush <jessicadawnbrush@gmail.com>
2018-05-15 13:36:57 -0700

    JS update

--- assets/javascript/feelin-fly.js
+++ assets/javascript/feelin-fly.js
@@ -17,33 +17,50 @@ $(document).on("click", "#submit-button", function () {
       type: "POST",
       data: {text : $("#watsonText").val()},
       success: function(response){
-        console.log(response);
-      
-      var sorted = false;
-        console.log(response.personality[4].percentile);
-      while (!sorted) {
 
-        sorted = true;
-        // Loop through the array
-        for (var i = 0; i < 5; i++) {
-          // If the current element is larger than the next element, swap them and set sorted to `false`
-          if (response.personality[3].percentile < response.personality[2].percentile) {
-            sorted = false;
-            var temp = (response.personality[i]);
-            (response.personality[i]) = (response.personality[i + 1]);
-            (response.personality[i + 1]) = temp;
+        $.ajax({
+          url: "https://shielded-castle-57250.herohuapp.com/countries",
+          type: "POST",
+          data: response,
+          success: function(results){
+            console.log(results);
+
           }
+
+        });
+        console.log(response);
+
+      
+      }
+    });
+  });
+
+
+      // var sorted = false;
+      //   console.log(response.personality[4].percentile);
+      // while (!sorted) {
+
+      //   sorted = true;
+      //   // Loop through the array
+      //   for (var i = 0; i < 5; i++) {
+      //     // If the current element is larger than the next element, swap them and set sorted to `false`
+      //     if (response.personality[3].percentile < response.personality[2].percentile) {
+      //       sorted = false;
+      //       var temp = (response.personality[i]);
+      //       (response.personality[i]) = (response.personality[i + 1]);
+      //       (response.personality[i + 1]) = temp;
+      //     }
             // console.log(response.personality[i].percentile);
             // console.log(response.personality[i].name);
-        }
+      //   }
 
-      }
+      // }
       // If we looped through the array without having to swap anything, exit the while loop and return the array
   
 
     
-  }
-  });
+  // }
+  // });
 
   // //feelinFly API
   // var queryURL = "https://feelinfly.com/countries"
@@ -56,6 +73,6 @@ $(document).on("click", "#submit-button", function () {
   //   console.log(response)
   // });
 
-});
+// });
 
   
